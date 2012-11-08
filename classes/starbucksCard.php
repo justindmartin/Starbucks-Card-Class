1a59468f822b4bb55695604e5e50a8193365ea79
Justin Martin <justin.d.martin1@gmail.com>
2012-11-08 17:39:49 -0600

    First Commit

--- classes/starbucksCard.php
+++ classes/starbucksCard.php
@@ -22,9 +22,4 @@ class starbucksCard{
 	    		return $match[0][0];
 	}
 }
-
-  //PROGRAM BODY
-  $myStarbucksCard = new starbucksCard('6069381974847839','01640273');
-  $balance = $myStarbucksCard->checkCardBalance();
-  echo 'Your card balance is '.$balance;
 ?>
