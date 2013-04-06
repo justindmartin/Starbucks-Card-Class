3780a32ba1571f93ac77d0dbca5e7846a806a37d
Justin Martin <justin.d.martin1@gmail.com>
2013-04-06 02:55:07 -0500

    Update check_balance_example.php

--- examples/check_balance_example.php
+++ examples/check_balance_example.php
@@ -1,7 +1,7 @@
 <?php
 require_once('../classes/starbucksCard.php');
 
-$myStarbucksCard = new starbucksCard('6069381974847839','01640273');
+$myStarbucksCard = new starbucksCard('STARBUCKS_CARD_NUMBER_HERE','STARBUCKS_CARD_PIN_HERE');
 $balance = $myStarbucksCard->checkCardBalance();
 echo 'Your card balance is '.$balance;
 ?>
